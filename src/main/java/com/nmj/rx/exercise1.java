7bd4bb51efeda0baa7bdbe861d266fcbdfc7fa93
neowiztomato <neowiztomato@neowiz.com>
2015-09-24 17:59:33 +0900

    rx code commit

---
+++ src/main/java/com/nmj/rx/exercise1.java
@@ -0,0 +1,13 @@
+package com.nmj.rx;
+
+public class exercise1 {
+	public static void main(String... argc) {
+		System.out.println("main start");
+
+		rx.Observable.just("first", "second", "third").map(v -> v + " time")
+		.subscribe(v -> System.out.println(v));
+		
+		System.out.println("main end..");
+	}
+
+}
